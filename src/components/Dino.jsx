e1e366034800290b85474b01447c7ef965753512
shekhar_codes <theshivshekhar0@gmail.com>
2025-11-22 15:01:12 +0530

    Dino rework

--- src/components/Dino.jsx
+++ src/components/Dino.jsx
@@ -1,10 +1,7 @@
 // src/components/Dinosaur.jsx
 import React, { forwardRef } from 'react';
 
-// The 'forwardRef' wraps your component function.
-// It receives 'props' and 'ref' as arguments.
 const Dino = forwardRef((props, ref) => (
-  // The ref from the parent is passed to the actual DOM element.
   <div ref={ref} className="absolute w-16 h-16" style={props.style}>
     <svg viewBox="0 0 100 100" fill="#555">
       <rect x="50" y="0" width="40" height="30" rx="5" />
